9962094e7b8419192267a9417b306029cf9b3d19
gabi <gabrielapmenezes@hotmail.com>
2018-09-20 20:10:33 -0300

    O mundo está ficando cruel!

--- 01/Main.py
+++ 01/Main.py
@@ -1,2 +1,17 @@
-print("oi,mundo!")
-print("oi,novamente!")
+import math
+
+a=int(input("Informe valor de a:"))
+b=int(input("Informe valor de b:"))
+c=int(input("Informe valor de c:"))
+
+delta = b * b - 4 * a * c
+
+print("Delta: ", delta)
+
+if delta >=0:
+    x1 = (-b + math.sqrt(delta)) / (2 * a)
+    x2 = (-b - math.sqrt)(delta)) / (2 * a)
+    print("Raiz x1: ", x1)
+    print("Raiz x2: ", x2)
+else:
+    print("Não existem raízes reais")
