f20f432dc1e7ac228b0f9df0b45a6a3b406db6c8
gabi <gabrielapmenezes@hotmail.com>
2018-09-20 21:47:20 -0300

    O mundo está ficando cruel!

---
+++ 05/ex.4.py
@@ -0,0 +1,27 @@
+n1= float(input("digite um número:"))
+n2= float(input("digite um número:"))
+n3= float(input("digite um número:"))
+
+if n1>n2>n3:
+    print("O maior número é",n1,"e o menor número é",n3)
+
+if n1>n3>n2:
+    print("O maior número é",n1, "e o menor número é",n2)
+
+if n2>n1>n3:
+    print("O maior número é", n2,"e o menor número é",n3)
+
+if n2>n3>n1:
+    print("O maior número é",n2,"e o menor número é",n1)
+
+if n3>n2>n1:
+    print("O maior número é",n3,"e o menor número é",n1)
+
+if n3>n1>n2:
+    print("O maior número é",n3,"e o menor número é",n1)
+
+
+
+
+
+
